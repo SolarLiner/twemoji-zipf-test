1f1e3d7b3c31233c283283a7070b2b0413a86e25
Nathan Graule <solarliner@gmail.com>
2017-08-27 01:43:42 +0700

    Added basic object creation tests for Tweet

--- src/tweet.spec.ts
+++ src/tweet.spec.ts
@@ -1,5 +1,15 @@
 import { assert } from "chai";
+import { Tweet } from "./tweet";
 
 it('Should create a new message object', () => {
-    
+    let tweetObject: Tweet;
+    assert.doesNotThrow(() => {
+        tweetObject = new Tweet('message');
+    })
+    assert.isNotNull(tweetObject);
+})
+it('Should implement timestamps correctly', () => {
+    let now = new Date(Date.now());
+    let tweetObject = new Tweet('message');
+    assert.isAtLeast(tweetObject.timestamp.valueOf(), now.valueOf());
 })
